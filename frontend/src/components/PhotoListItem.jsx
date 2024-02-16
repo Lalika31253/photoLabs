ee7c87bed0f45bfccca9dda8ba9c94bb9e887b9f
Lalika31253 <Lalika31253@gmail.com>
2024-02-16 12:39:20 -0700

    Correct styling of the PhotoListItem component

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -6,23 +6,20 @@ const PhotoListItem = ({ data }) => {
 const { id, location, imageSource, username, profile } = data;
 
 return (
-  <div className="photo-list__item">
-
-    <div className="photo-list__image">
-      <img src={imageSource} alt="User image" />
-
-     <div className="photo-list__user-details">
-       <img className="photo-list__user-profile" src={profile} alt="User profile" />
-        <div className="photo-list__user-info"> 
-          {username}
-            <div className="photo-list__user-info photo-list__user-location">
-              {location.city}
-              {location.country}
-           </div>
-        </div>   
-     </div>
-    </div>
+<div className="photo-list__item">
+  <img className="photo-list__image" src={imageSource} alt="User image" />
+  <div className="photo-list__user-details">
+    <img className="photo-list__user-profile" src={profile} alt="User profile" />
+    <div className="photo-list__user-info"> 
+      {username}
+      <div className="photo-list__user-location">
+        {location.city}
+        {location.country}
+      </div>
+    </div>   
   </div>
+</div>
+
 )
 }
 
