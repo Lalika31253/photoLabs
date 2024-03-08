810f57c33585925a04c43f565551130fbca530e8
Lalika31253 <Lalika31253@gmail.com>
2024-03-08 00:21:00 -0700

    Take out quotes around of the ACTIONS object

--- frontend/src/hooks/useApplicationData.js
+++ frontend/src/hooks/useApplicationData.js
@@ -52,9 +52,9 @@ const useApplicationData = () => {
   const handleFavourite = (id) => {
     const actionType = state.favourite.includes(id);
     if(actionType) {
-      dispatch({type: 'ACTIONS.FAV_PHOTO_REMOVED', payload: id});
+      dispatch({type: ACTIONS.FAV_PHOTO_REMOVED, payload: id});
     } else {
-      dispatch({type: 'ACTIONS.FAV_PHOTO_ADDED', payload: id});
+      dispatch({type: ACTIONS.FAV_PHOTO_ADDED, payload: id});
     }
   };
 
@@ -62,7 +62,7 @@ const useApplicationData = () => {
   const updateDisplayModal = (props) => {
     const modalState = state.displayModal ? false : props;
     // const modalState = !state.displayModal;
-    dispatch({type: 'ACTIONS.DISPLAY_PHOTO_DETAILS', payload: modalState});
+    dispatch({type: ACTIONS.DISPLAY_PHOTO_DETAILS, payload: modalState});
   };
 
 
@@ -71,31 +71,6 @@ const useApplicationData = () => {
     {favourite: state.favourite, displayModal: state.displayModal, handleFavourite, updateDisplayModal}
   )
 
-
-
-  // const [favourite, setFavourite] = useState([]);
-
-  // const handleFavourite = (id) => {
-  //   setFavourite((presentFavourites) => {
-  //     if (presentFavourites.includes(id)) {
-  //       return presentFavourites.filter(favouriteId => favouriteId !== id);
-  //     } else {
-  //       return [...presentFavourites, id]
-  //     }
-  //   })
-  // }
-
-  // const [displayModal, setDisplayModal] = useState(null);
-
-  // const updateDisplayModal = (props) => {
-  //   const modalProps = { ...props, modalState: true };
-  //   displayModal ? setDisplayModal(null) : setDisplayModal(modalProps);
-  
-  // }
-
-  // return (
-  //   { favourite, handleFavourite, displayModal, updateDisplayModal }
-  // )
 
 };
 
